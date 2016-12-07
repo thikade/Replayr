296907ec69ebcc4b982e004fde4ed1b80d8a8e7c
Thomas Hikade <thikade@users.noreply.github.com>
2016-12-07 14:06:30 +0100

    multiple changes
    - switched to Apache combined logformat
    - --server ARG will now be used to contruct the HTTP url instead of using a proxy
    - does not tail the log! Processes logfile, then terminates
    - only POST requests are ignored

--- replayr.py
+++ replayr.py
@@ -1,6 +1,7 @@
 """Replayr -- a realtime HTTP log replay script.
 
 Copyright(c) 2011 Adrian Lienhard <adrian@cmsbox.com>
+Copyright(c) 2011 Thomas Hikade <thomashikade@gmail.com>
 MIT Licensed
 """
 
@@ -13,10 +14,11 @@ import time
 import re
 
 # Apache log format -- you may need to adapt regex to your custom format!
-# This pattern matches the Apache format
-#"%{Host}i %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"" vcombined
+# This pattern matches the Apache combined format
+# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+
 LOGLINE_RE = re.compile(\
-    r'(?P<host>\S+) (?P<ip>\S+) (\S+) (\S+) \[(?P<time>.*?)\] "(?P<method>\S+) (?P<path>\S+) (?P<version>\S+)" (?P<code>\S+) (?P<size>\S+) "(?P<useragent>\S+)"')
+    r'(?P<ip>\S+) (\S+) (\S+) \[(?P<time>.*?)\] "(?P<method>\S+) (?P<path>\S+) (?P<version>\S+)" (?P<code>\S+) (?P<size>\S+)')
 
 # HTTP client timeout in seconds
 TIMEOUT = 10
@@ -24,30 +26,41 @@ TIMEOUT = 10
 
 class RedirectHandler(urllib2.HTTPRedirectHandler):
     """Custom redirect handler that does not follow 301/302 responses"""
-    
-    def http_error_301(self, req, fp, code, msg, headers):  
-        result = urllib2.HTTPRedirectHandler.http_error_301( 
+
+    def http_error_301(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPRedirectHandler.http_error_301(
             self, req, fp, code, msg, headers)
         result.status = code
-    
+
     def http_error_302(self, req, fp, code, msg, headers):
         result = urllib2.HTTPRedirectHandler.http_error_302(
             self, req, fp, code, msg, headers)
         result.status = code
-    
-    
-def main(f, proxy, exclude):
+
+
+def main(f, host, exclude):
     """Setup HTTP client and generator pipe for requests"""
-    setup_http_client(proxy)
+    # setup_http_client(host)
     loglines = follow(f)
     requests = (parse(line) for line in loglines)
-    requests = (r for r in requests if r is not None)
+
     if exclude:
         pattern = re.compile(exclude)
-        requests = (r for r in requests if not pattern.search(r['path']))
-    requests = (r for r in requests if is_get_or_head_request(r))
-    for r in requests:
-        send_request(r)
+
+    for line in loglines:
+        r = parse(line)
+        if r is None:
+            print "not matched: " + repr(line)
+            continue
+        if exclude and pattern.search(r['path']):
+            print "filtered: " + repr(line)
+            continue
+
+        if not is_replayable_request(r):
+            print "cannot replay: %s %s  [status: %s]" % (r['method'], r['path'],r['code'])
+            continue
+
+        send_request(r, host)
 
 def parse(line):
     """Parse a log line, return None if regex does not match."""
@@ -56,13 +69,14 @@ def parse(line):
         return match.groupdict()
     return None
 
-def is_get_or_head_request(r):
-    """Filter GET and HEAD requests."""
-    return r['method'] == 'GET' or r['method'] == 'HEAD'
-    
-def send_request(r):
+def is_replayable_request(r):
+    """Filter POST requests."""
+    # return r['method'] == 'GET' or r['method'] == 'HEAD'
+    return r['method'] != 'POST'
+
+def send_request(r, host):
     """Send the request r"""
-    url = "http://" + r['host'] + r['path']
+    url = "http://" + host + r['path']
     req_result = None
     req_start = datetime.now()
     code = '200'
@@ -75,13 +89,13 @@ def send_request(r):
         code = str(e.code)
     except urllib2.URLError, e:
         req_result = 'FAILED[URLError: ' + str(e.reason) + ']'
-    
-    # treat 304 as a 200 status code since it depends on client cache 
+
+    # treat 304 as a 200 status code since it depends on client cache
     expected_code = r['code'] if r['code'] != '304' else '200'
     if not req_result and code != expected_code:
         req_result = 'FAILED[' + code + ' but expected ' + expected_code + ']'
     else:
-        req_result = 'OK[' + code + ']' 
+        req_result = 'OK[' + code + ']'
 
     req_delta = datetime.now() - req_start
     req_msecs = req_delta.seconds * 1000 + req_delta.microseconds / 1000
@@ -94,13 +108,14 @@ def setup_http_client(proxy):
     proxy_handler = urllib2.ProxyHandler(proxy_config)
     opener = urllib2.build_opener(proxy_handler, RedirectHandler)
     urllib2.install_opener(opener)
-   
+
 def follow(f):
     """A generator that yields new lines in file f."""
     while True:
         line = f.readline()
         if not line:
-            time.sleep(0.1)
+            break
+            # time.sleep(0.1)
             continue
         yield line
 
@@ -120,7 +135,7 @@ if __name__ == "__main__":
     if len(args) == 1:
         # open file and move to end -- behaves like "tail -f"
         f = open(args[0])
-        f.seek(0,2)
+        # f.seek(0,2)
         main(f, options.host, options.regex)
     elif len(args) == 0:
         main(sys.stdin, options.host, options.regex)
